f8cd01d59e7771e43ed22c069ea9236f25a1829f
Anderain <anderuri1@gmail.com>
2020-03-10 16:49:01 +0100

    Ejercicio3

--- doc.java
+++ doc.java
@@ -1,14 +1,14 @@
 
-/**
- * Write a description of class doc here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public interface doc
 {
    
-   // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
+   /**
+    * Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
+    * 
+    * @param int x El numero de la fila
+    * @param String file El fichero de texto con la ruta absoluta
+    * @return El contenido del fichero file en la fila x
+    */ 
    public abstract String obtenerLinea(int x, String file);
    
    
