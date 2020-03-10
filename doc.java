28c33cab6da70593f02147ae8efd7ed016a53b15
Anderain <anderuri1@gmail.com>
2020-03-10 16:57:05 +0100

    Ejercicio4

--- doc.java
+++ doc.java
@@ -10,6 +10,11 @@ public interface doc
     * @return El contenido del fichero file en la fila x
     */ 
    public abstract String obtenerLinea(int x, String file);
-   
-   
+   /**
+    * Devuelve la posición (latitud y altitud) dada una direccion.
+    * 
+    * @param String calle La direccion de la que queremos saber la posición.
+    * @return La posición en (latitud y altitud) de la calle.
+    */
+   public abstract float obtenerPosicion(String calle);
 }
